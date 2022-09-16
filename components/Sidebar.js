079d8e77806aa49f415ca08647709b3fc3cc259e
Zulhilmi Rahmat <92867326+ZulAmi@users.noreply.github.com>
2022-09-16 16:16:51 +0800

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+function Sidebar() {
+    return (
+        <Container>
+            <h1>Sidebar</h1>
+        </Container>
+    )
+}
+
+export default Sidebar;
+
+const Container = styled.div`
+`
