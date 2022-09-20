dbca890be780177f4276568ad58635e071f2c05b
Zulhilmi Rahmat <92867326+ZulAmi@users.noreply.github.com>
2022-09-20 16:36:17 +0800

    added sign in with google via firebase

--- pages/login.js
+++ pages/login.js
@@ -2,8 +2,13 @@ import { Link } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import Head from "next/head";
 import styled from "styled-components";
+import { auth, provider } from "../firebase"
 
 function Login() {
+
+    const signIn = () => {
+        auth.signInWithPopup(provider).catch(alert);
+    };
     return (
         <Container>
             <Head>
@@ -12,7 +17,7 @@ function Login() {
 
             <LoginContainer>
                 <Logo src="https://img.freepik.com/free-vector/japanese-samurai-with-black-mask-japan-warrior-flat-image-vintage-vector-illustration_74855-11316.jpg?w=740&t=st=1663590059~exp=1663590659~hmac=1cf3554da13f9b8389c8edb3f12b8c62f1490ccb9655d753561f6fc787b9d6f7" alt="" />
-                <Button variant="outlined">Sign in with Google</Button>
+                <Button onClick={signIn} variant="outlined">Sign in with Google</Button>
             </LoginContainer>
         </Container>
     )
@@ -24,16 +29,22 @@ const Container = styled.div`
 display: grid;
 place-items: center;
 height: 100vh;
+background-color: whitesmoke;
 `;
 
 const LoginContainer = styled.div`
+padding: 100px;
 display: flex;
 flex-direction: column;
+align-items: center;
+background-color: white;
+border-radius: 5px;
+box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
 `;
 
 const Logo = styled.img`
 height: 200px;
 width: 200px;
-margin-bottom: 25px;
+padding-bottom: 10px;
 `;
 
