ebb3fdd290e9008c1b987e2b652efccd1cfad397
Zulhilmi Rahmat <92867326+ZulAmi@users.noreply.github.com>
2022-09-22 10:27:46 +0800

    added picture and stylized chat component

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
 
 function Chat({ id, users }) {
     return (
         <Container>
+            <UserAvatar />
             <p> Recipient Email </p>
         </Container>
     )
@@ -10,5 +12,18 @@ function Chat({ id, users }) {
 
 export default Chat;
 
-const Container = styled.div``;
+const Container = styled.div`
+display: flex;
+align-items: center;
+cursor: pointer;
+padding: 15px;
+word-break: break-word;
+
+:hover { background-color: #e9eae9}
+`;
+
+const UserAvatar = styled(Avatar)`
+    margin-right: 15px;
+    margin : 5px;
+`;
 
