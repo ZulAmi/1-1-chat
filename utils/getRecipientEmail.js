f3012cc84aa69a1725f3cc01f4335473af65e4ff
Zulhilmi Rahmat <92867326+ZulAmi@users.noreply.github.com>
2022-09-29 21:31:01 +0800

    modified getrecipientemail.js

--- utils/getRecipientEmail.js
+++ utils/getRecipientEmail.js
@@ -1,4 +1,4 @@
 const getRecipientEmail = (user, userLoggedIn) =>
-    users?.filter(userToFilter => userToFilter !== userLoggedIn?.email)[0];
+    users?.filter((userToFilter) => userToFilter !== userLoggedIn?.email)[0];
 
 export default getRecipientEmail;
