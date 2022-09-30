f6e5212cb45e199319744f8df3bc4a465fea5433
Zulhilmi Rahmat <92867326+ZulAmi@users.noreply.github.com>
2022-09-30 16:54:18 +0800

    resolved getrecipientemail.js and chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -5,15 +5,16 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase";
 
 function Chat({ id, users }) {
-    const [user] = useAuthState(auth)
+    const [user] = useAuthState(auth);
+
     const recipientEmail = getRecipientEmail(users, user);
 
     return (
         <Container>
             <UserAvatar />
-            <p> Recipient Email </p>
+            <p>{recipientEmail} </p>
         </Container>
-    )
+    );
 }
 
 export default Chat;
--- utils/getRecipientEmail.js
+++ utils/getRecipientEmail.js
@@ -1,4 +1,4 @@
 const getRecipientEmail = (user, userLoggedIn) =>
-    users?.filter((userToFilter) => userToFilter !== userLoggedIn?.email)[0];
+    user?.filter((userToFilter) => userToFilter !== userLoggedIn?.email)[0];
 
 export default getRecipientEmail;
